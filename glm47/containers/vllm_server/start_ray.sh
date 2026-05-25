9d7f72f0c339a6689564c5726bd7acd759226102
Neil Horman <nhorman@openssl.org>
2026-05-25 05:25:09 -0400

    auto start vllm and remove ollama
    We don't need the latter

--- glm47/containers/vllm_server/start_ray.sh
+++ glm47/containers/vllm_server/start_ray.sh
@@ -2,9 +2,22 @@
 #
 source ~/vllm-latest/bin/activate
 
+export HF_TOKEN=$(cat /run/secrets/hf_token)
+
 if [ "$RAY_MODE" == "head" ]
 then
-	ray start --block --head --node-ip-address=${VLLM_HOST_IP} --port=6379 --dashboard-host=0.0.0.0
+	ray start --head --node-ip-address=${VLLM_HOST_IP} --port=6379 --dashboard-host=0.0.0.0
+	while true; do
+		NODE_COUNT=$(ray status | awk '/node_/ {print $0}' | wc -l)
+		if [ $NODE_COUNT -ne 2 ]; then
+			echo "Waiting for 2 nodes, have $NODE_COUNT"
+			sleep 1
+		else
+			break
+		fi
+	done
+	echo "Have 2 nodes, starting vllm with GLM-4.7-Flash model"
+	vllm serve "zai-org/GLM-4.7-Flash" --host 0.0.0.0 --port 8000 --tensor-parallel-size=1 --pipeline-parallel-size=2 --distributed-executor-backend ray --gpu-memory-utilization=0.7 --max-model-len=-1 --enable-auto-tool-choice --tool-call-parser glm47
 else
-	ray start --block --node-ip-address=${VLLM_HOST_IP} --address=${MASTER_ADDR}:6379 
+	ray start -block --node-ip-address=${VLLM_HOST_IP} --address=${MASTER_ADDR}:6379 
 fi
